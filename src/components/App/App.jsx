58eab19158ed0d40ee4710221a909e015ea622f9
nicbeuc <nick.beuchat@gmail.com>
2023-03-18 10:26:45 -0400

    initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Schema Builder</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/constants.js
@@ -0,0 +1,85 @@
+/*
+  Color themes, breakpoints, public
+  keys, and other app-wide constants.
+*/
+
+export const INPUT_TYPES = {
+  'checkbox': {
+    'name': 'Checkbox',
+  },
+  'number': {
+    'name': 'Number',
+  },
+  'radio': {
+    'name': 'Radio',
+  },
+  'range': {
+    'name': 'Range',
+  },
+  'select': {
+    'name': 'Select',
+  },
+  'text': {
+    'name': 'Text',
+  },
+  'textarea': {
+    'name': 'Text area',
+  },
+  'article': {
+    'name': 'Article',
+  },
+  'blog': {
+    'name': 'Blog',
+  },
+  'collection': {
+    'name': 'Collection',
+  },
+  'collection_list': {
+    'name': 'Collection list',
+  },
+  'color': {
+    'name': 'Color',
+  },
+  'color_background': {
+    'name': 'CSS Color',
+  },
+  'font_picker': {
+    'name': 'Font',
+  },
+  'html': {
+    'name': 'HTML',
+  },
+  'image_picker': {
+    'name': 'Image',
+  },
+  'inline_richtext': {
+    'name': 'Inline rich text',
+  },
+  'link_list': {
+    'name': 'Link list',
+  },
+  'liquid': {
+    'name': 'Custom liquid',
+  },
+  'page': {
+    'name': 'Page',
+  },
+  'product': {
+    'name': 'Product',
+  },
+  'product_list': {
+    'name': 'Product list',
+  },
+  'richtext': {
+    'name': 'Rich text'
+  },
+  'url': {
+    'name': 'URL',
+  },
+  'video': {
+    'name': 'Video',
+  },
+  'video_url': {
+    'name': 'Video URL',
+  }
+}
